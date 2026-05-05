74171052bff708c8f70f0efa42838edc3e1cd963
retaj-aljbour <retajalnoor08@gmail.com>
2026-05-05 21:36:20 +0300

    add footer to layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import About from "./pages/About";
-//import "./styles.css";
+
 import "./App.css";
 function App() {
   return (
@@ -14,12 +14,15 @@ function App() {
         <Navbar />
 
         <Routes>
+    
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/about" element={<About />} />
         </Routes>
-
+<footer>
+  <p>Movie App Project © 2026</p>
+</footer>
       </div>
     </Router>
   );
